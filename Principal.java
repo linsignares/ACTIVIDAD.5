38f02906816e560e09f030320abe441f662f04ac
linsignares <142268649+linsignares@users.noreply.github.com>
2023-11-13 18:15:55 -0500

    Principal.java

---
+++ Principal.java
@@ -0,0 +1,11 @@
+/**
+ *
+ * @author julianainsignares
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal= new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true);
+    }
+}
